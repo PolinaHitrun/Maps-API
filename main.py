88e01b6aff1cc6b914ae3a6feb4927d914b5c507
student <geba_m@mail.ru>
2021-02-12 17:22:24 +0700

    Two commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
 import requests
-
+from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 SCREEN_SIZE = 600, 450
 
@@ -10,15 +10,13 @@ SCREEN_SIZE = 600, 450
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
+        uic.loadUi('main.ui', self)
         self.initUI()
 
     def initUI(self):
         self.setGeometry(50, 50, 620, 470)
         self.getImage()
         self.pixmap = QPixmap('map_file.png')
-        self.image = QLabel(self)
-        self.image.move(10, 10)
-        self.image.resize(*SCREEN_SIZE)
         self.image.setPixmap(self.pixmap)
 
     def getImage(self):
