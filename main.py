e165ae20ef647a3de18df7c83bbde74fc8ad4e32
M8r1na <dm.mar@list.ru>
2021-02-12 18:29:32 +0700

    масштаб

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys, requests, os
+import sys
+import requests
 from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow
@@ -12,15 +13,21 @@ class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
-        self.radioButtonGroup.buttonClicked.connect(self.setView)
-        self.l, self.spn, self.ll = 'map', '0.05', '37.677751,55.757718'
+        self.initUI()
+
+        self.PgUpButton.clicked.connect(self.up_scale)
+        self.PgDownButton.clicked.connect(self.down_scale)
+        self.scale = 10
+
+
+    def initUI(self):
         self.setGeometry(50, 50, 620, 470)
         self.getImage()
         self.pixmap = QPixmap('map_file.png')
         self.image.setPixmap(self.pixmap)
 
-    def getImage(self):
-        map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.ll}&spn={self.spn},{self.spn}&l={self.l}"
+    def getImage(self, ll, spn, l):
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={ll}&z={self.scale}&spn={spn},{spn}&l={l}"
         self.response = requests.get(map_request)
         with open('map_file.png', "wb") as file:
             file.write(self.response.content)
@@ -35,17 +42,13 @@ class Example(QMainWindow):
         elif event.key() == Qt.Key_Right:
             pass
 
-    def setView(self, button):
-        if button == self.radioButton1:
-            self.l = 'map'
-        if button == self.radioButton2:
-            self.l = 'sat'
-        if button == self.radioButton3:
-            self.l = 'skl'
-        os.remove('map_file.png')
-        self.getImage()
-        self.pixmap = QPixmap('map_file.png')
-        self.image.setPixmap(self.pixmap)
+    def up_scale(self):
+        if self.scale < 17:
+            self.scale += 1
+
+    def down_scale(self):
+        if self.scale > 0:
+            self.scale -= 1
 
 
 if __name__ == '__main__':
