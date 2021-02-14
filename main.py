9e4c0cb7c4e5637a3070176fcd0ae31db8651fc2
geba- <geba_m@mail.ru>
2021-02-14 11:30:11 +0700

    KeyPress

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import sys
 import requests
-from PyQt5 import uic
+from PyQt5 import uic, QtCore
 from PyQt5.QtGui import QPixmap
 import sys, requests, os
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
 from PyQt5.QtCore import Qt
 
 SCREEN_SIZE = 600, 450
@@ -14,18 +13,11 @@ class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
-        self.initUI()
-
+        self.setGeometry(50, 50, 620, 470)
+        self.l, self.spn, self.ll, self.scale = 'map', '0.05', '37.677751,55.757718', 10
         self.radioButtonGroup.buttonClicked.connect(self.setView)
-        self.l, self.spn, self.ll = 'map', '0.05', '37.677751,55.757718'
-
         self.PgUpButton.clicked.connect(self.up_scale)
         self.PgDownButton.clicked.connect(self.down_scale)
-        self.scale = 10
-
-
-    def initUI(self):
-        self.setGeometry(50, 50, 620, 470)
         self.getImage()
         self.pixmap = QPixmap('map_file.png')
         self.image.setPixmap(self.pixmap)
@@ -37,14 +29,19 @@ class Example(QMainWindow):
             file.write(self.response.content)
 
     def keyPressEvent(self, event):
+        self.y, self.x = [float(x) for x in self.ll.split(',')]
         if event.key() == Qt.Key_Up:
-            pass
+            self.x += float(self.spn) * 2
         elif event.key() == Qt.Key_Down:
-            pass
+            self.x -= float(self.spn) * 2
         elif event.key() == Qt.Key_Left:
-            pass
+            self.y -= float(self.spn) * 2
         elif event.key() == Qt.Key_Right:
-            pass
+            self.y += float(self.spn) * 2
+        self.ll = f'{str(self.y)},{str(self.x)}'
+        self.getImage()
+        self.pixmap = QPixmap('map_file.png')
+        self.image.setPixmap(self.pixmap)
 
     def up_scale(self):
         if self.scale < 17:
@@ -67,8 +64,13 @@ class Example(QMainWindow):
         self.image.setPixmap(self.pixmap)
 
 
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     ex.show()
+    sys.excepthook = except_hook
     sys.exit(app.exec())
